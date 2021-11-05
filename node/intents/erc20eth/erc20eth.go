41b940a3e9d492ee31c797f2b6e7ba8af698ff95
turtlebasket <turtlebasket@gmail.com>
2021-11-05 13:54:11 -0700

    find verified ERC20 intents, pending TODO

--- node/intents/erc20eth/erc20eth.go
+++ node/intents/erc20eth/erc20eth.go
@@ -1,8 +1,12 @@
 package erc20eth
 
 import (
+	"math/big"
+	"time"
 	"xnode/intents"
+	"xnode/intents/erc20eth/erc20abi"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -10,13 +14,19 @@ type ERC20EthProcessor struct {
 	Id              string
 	ContractAddress string
 	Client          *ethclient.Client
+	Contract        *erc20abi.ERC20
 }
 
 func NewERC20EthProcessor(id string, contractAddress string, client *ethclient.Client) *ERC20EthProcessor {
+	contract, err := erc20abi.NewERC20(common.HexToAddress(contractAddress), client)
+	if err != nil {
+		panic(err)
+	}
 	return &ERC20EthProcessor{
 		Id:              id,
 		ContractAddress: contractAddress,
 		Client:          client,
+		Contract:        contract,
 	}
 }
 
@@ -27,8 +37,27 @@ func (p *ERC20EthProcessor) CurrencyId() string {
 func (p *ERC20EthProcessor) Process(intent *intents.PaymentIntent) chan intents.PaymentStatus {
 	status := make(chan intents.PaymentStatus)
 
-	go func() {
+	amtInt64, _ := intent.Amount.Int64()
+	amtEth := big.NewInt(amtInt64)
+	toEth := common.HexToAddress(intent.To)
 
+	// Pending transaction loop
+	// NOTE: FIND OUT HOW TO GET PENDING BALANCES FROM ERC20ABI BINDINGS
+	go func() {
+	}()
+
+	// Verified transaction loop
+	go func() {
+	verifiedLoop:
+		for {
+			balance, _ := p.Contract.BalanceOf(nil, toEth)
+			comparison := balance.Cmp(amtEth)
+			if comparison == 1 || comparison == 0 {
+				status <- intents.Verified
+				break verifiedLoop
+			}
+			time.Sleep(1 * time.Second)
+		}
 	}()
 
 	return status
